effe29451fff114d81abffd9c0c27fffeea05e8c
Richard Bankole <rkb@fb.com>
2023-04-17 18:15:05 +0200

    Add basic password implementation to Admin Page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,12 +2,19 @@
 import React, { useEffect, useState } from "react";
 import CategoryList from "../../components/CategoryList/CategoryList";
 import { getCategories } from "../../pages/api";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography, TextField } from "@mui/material";
 import styles from "./AdminPage.module.css";
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const AdminPage: React.FC = () => {
     const [categories, setCategories] = useState<any[]>([]);
+    const [password, setPassword] = useState("");
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const router = useRouter();
+
+    const ADMIN_PASSWORD = "test123";
+
     useEffect(() => {
         fetchCategories();
     }, []);
@@ -44,25 +51,52 @@ const AdminPage: React.FC = () => {
         fetchCategories(true);
     };
 
+    const handlePasswordSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (password === ADMIN_PASSWORD) {
+            setIsAuthenticated(true);
+        } else {
+            router.push("/");
+        }
+    };
+
+
     return (
         <Container>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2 }}>
-                <Typography variant="h3" gutterBottom>Admin Page</Typography>
-                <Box mt={2} mb={4}>
-                    <Button
-                        className={styles["fetch-button"]}
-                        onClick={() => fetchCategories(true)}
-                        variant="contained"
-                    >
-                        Fetch Latest Categories
-                    </Button>
+            {isAuthenticated ? (
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2 }}>
+                    <Typography variant="h3" gutterBottom>Admin Page</Typography>
+                    <Box mt={2} mb={4}>
+                        <Button
+                            className={styles["fetch-button"]}
+                            onClick={() => fetchCategories(true)}
+                            variant="contained"
+                        >
+                            Fetch Latest Categories
+                        </Button>
+                    </Box>
+                    <CategoryList
+                        categories={categories}
+                        fetchCategories={fetchCategories}
+                        updateQuestionText={updateQuestionText}
+                    />
                 </Box>
-                <CategoryList
-                    categories={categories}
-                    fetchCategories={fetchCategories}
-                    updateQuestionText={updateQuestionText}
-                />
-            </Box>
+            ) : (
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2 }}>
+                    <Typography variant="h4" gutterBottom>Admin Login</Typography>
+                    <form onSubmit={handlePasswordSubmit}>
+                        <TextField
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            sx={{ mb: 2 }}
+                        />
+                        <Button type="submit" variant="contained">Login</Button>
+                    </form>
+                </Box>
+            )}
         </Container>
     );
 };
